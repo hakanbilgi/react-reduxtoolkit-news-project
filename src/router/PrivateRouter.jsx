e6e312be742b75a1679528f9585cd2b223651fac
Hakan Bilgi <hknblg34@gmail.com>
2022-11-14 18:41:28 +0300

    consume

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,7 +1,9 @@
+import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouter = () => {
-  const user = true; //! TODO: useSelector
+  // const user = useSelector((state)=> state.auth.user);
+  const {user} = useSelector((state)=> state.auth); //! desc.
 
   return user ? <Outlet /> : <Navigate to="/login" />;
 };
