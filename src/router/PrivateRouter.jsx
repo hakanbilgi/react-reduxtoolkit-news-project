607cf6d802ff0a65c4fffe5707c0c6fb2554c3b9
Hakan Bilgi <hknblg34@gmail.com>
2022-11-18 17:05:02 +0300

    private

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -3,6 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import authSlice from "../features/authSlice";
 
 const PrivateRouter = () => {
+  
   const { user } = useSelector((state) => state.auth);
 
   console.log(user);
